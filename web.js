daa8bf131ce0555efa721e380e98cdd37d46b21c
Matt McQuarrie <matmcq@gmail.com>
2013-07-10 17:36:48 -0600

    Updated to print from file

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var buffer = new Buffer(16);
+buffer = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
